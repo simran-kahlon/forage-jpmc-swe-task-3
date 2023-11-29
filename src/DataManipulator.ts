fb71d04a95bf4a611adc51d300e65d4319faab7c
Simran Kahlon <simran@simrans-mbp.mynetworksettings.com>
2023-11-29 18:35:35 -0500

    graph changed to show ratio, upper bound, lower bound, and alerts when the bounds are crossed

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const price_abc = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2;
+    const price_def = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2;
+    const ratio = price_abc/price_def;
+    const upper_bound = 1 + 0.05;
+    const lower_bound = 1 - 0.05;
+    return {
+      price_abc:  price_abc,
+      price_def: price_def,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upper_bound,
+      lower_bound: lower_bound,
+      trigger_alert: (ratio > upper_bound || ratio < lower_bound) ? ratio: undefined,
+    };
   }
 }
